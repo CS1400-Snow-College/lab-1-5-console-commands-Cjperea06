4755b18a77f8b82902d157b2058fd3185b08afab
Cj Perea <cjperea@Cjs-MacBook-Pro.local>
2025-06-04 17:35:47 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel;
+using System.Reflection.Metadata;
+
+Console.Clear();
+Console.BackgroundColor - ConsoleColor.Cyan;
+Console.ForegroundColor - ConsoleColor.Cyan;
+Console.SetCursorPosition(0, 0);
+Console.WriteLine(" ");
